41b471226c8bf904ff01d09c15f2271007a142cd
RS096293 <Rajesh.Singh@cerner.com>
2025-05-29 19:42:57 +0530

    Initial commit

---
+++ src/main/java/io/github/rajeshcse1721/journalApp/JournalApplication.java
@@ -0,0 +1,13 @@
+package io.github.rajeshcse1721.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/github/rajeshcse1721/journalApp/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package io.github.rajeshcse1721.journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/heath-check")
+    public String healthCheck(){
+        return "Ok";
+    }
+}
